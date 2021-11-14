62657e48d7d5d9e9f9431743f3ad3364ede06782
Micha <micha.pdm@gmx.de>
2021-11-14 20:14:42 +0100

    feat: added 3x sensor, posts values to server

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+const cors = require("cors");
+
+var app = express();
+
+app.use(cors()); //Allowing cors for all origins
+
+const port = process.env.PORT || 3000;
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/sensor');
+
+
+const {
+    greeting,
+    sensor,
+} = require('./controllers/controllers');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', greeting);
+app.use('/greeting', greeting);
+app.post('/sensor', sensor);
+
+app.listen(port, () => { 
+  console.log(`Backend app listening at http://localhost:${port}`) 
+})
+
+module.exports = app;
---
+++ controllers/controllers.js
@@ -0,0 +1,35 @@
+const greeting = async (req, res, next) => {
+    console.log("Greeing...");
+    const greetingText = "Welcome to the backend";
+
+    try {
+        res.json(greetingText);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+const sensor = async (req, res, next) => {
+    // console.log("Sensor...");
+    const { data } = req. body;
+    const { time, pressure, temperature, humidity, carbondioxide, organic } = req. body;
+    
+    console.log(`Pressure: ${pressure} hPa  Temp: ${temperature} °C  Humidtiy: ${humidity} %  CO2: ${carbondioxide} ppm  tVOC: ${organic} ppb`)
+    
+    // console.log(time);
+    // console.log(data);
+    const greetingText = "Sensor";
+
+    try {
+        res.json(greetingText);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+module.exports = {
+    greeting,
+    sensor,
+};
