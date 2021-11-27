7b61f80058ff246af8a4904ed5a034848383bb16
Micha <micha.pdm@gmx.de>
2021-11-27 19:53:19 +0100

    refactor

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ var app = express();
 
 app.use(cors()); //Allowing cors for all origins
 
+/**************************************************/
 const port = process.env.PORT || 3000;
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/sensor');
 require("./database/client");
 var usersRouter = require('./routes/users');
 var sensordataRouter = require('./routes/sensordata');
-var measuredDataRouter = require('./routes/measuredData');
+var measuredDataRouter = require('./routes/measurements');
 
+/**************************************************/
 const {
   greeting,
   sensor,
@@ -25,12 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/greeting', greeting);
-app.post('/sensor', sensor);
-app.use('/users', usersRouter);
-app.use('/sensordata', sensordataRouter);
-app.use('/measurements', measuredDataRouter);
+app.post('/sensor', sensor);                  // interface for the sensor - sensor sends data to here
+app.use('/greeting', greeting);               // ToDo
+app.use('/users', usersRouter);               // ToDo
+app.use('/sensordata', sensordataRouter);     // to create and list sensors
+app.use('/measurements', measuredDataRouter); // to get measurements
 
+
+/**************************************************/
 app.listen(port, () => {
   console.log(`Backend app listening at http://localhost:${port}`)
 })
